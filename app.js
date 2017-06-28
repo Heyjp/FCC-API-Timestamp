eff79c47977c46b2e80df5daedcbf5b63e478289
Jamie Player <jamieplayer@gmail.com>
2017-06-28 10:49:17 +0100

    added port env var for heroku

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.get('/api/:id', function(req, res){
   res.json(dateAssembler(request));
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
